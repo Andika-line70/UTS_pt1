8f3ea1f2473cf66779e7107b51c845df4dc43cb3
Andika-Ctrl_c <aprasetya980@gmail.com>
2026-04-16 21:49:43 +0700

    Bubble_sort.java

--- Bubble_sort.java
+++ Bubble_sort.java
@@ -1,21 +1,33 @@
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] nilai = {78, 82, 75, 88, 90, 84, 79, 85, 87, 80};
-        int temp;
+        int[] data = {78, 82, 75, 88, 90, 84, 79, 85, 87, 80};
 
-        for (int i = 0; i < nilai.length - 1; i++) {
-            for (int j = 0; j < nilai.length - 1 - i; j++) {
-                if (nilai[j] > nilai[j + 1]) {
-                    temp = nilai[j];
-                    nilai[j] = nilai[j + 1];
-                    nilai[j + 1] = temp;
+        System.out.println("Data awal:");
+        tampil(data);
+
+        System.out.println("\nProses Bubble Sort:");
+
+        for (int i = 0; i < data.length - 1; i++) {
+            for (int j = 0; j < data.length - 1 - i; j++) {
+                if (data[j] > data[j + 1]) {
+                    int temp = data[j];
+                    data[j] = data[j + 1];
+                    data[j + 1] = temp;
                 }
             }
+
+            System.out.print("Iterasi " + (i + 1) + ": ");
+            tampil(data);
         }
 
-        System.out.println("Hasil Bubble Sort:");
-        for (int n : nilai) {
-            System.out.print(n + " ");
+        System.out.println("\nHasil akhir:");
+        tampil(data);
+    }
+
+    static void tampil(int[] data) {
+        for (int i = 0; i < data.length; i++) {
+            System.out.print(data[i] + " ");
         }
+        System.out.println();
     }
 }
